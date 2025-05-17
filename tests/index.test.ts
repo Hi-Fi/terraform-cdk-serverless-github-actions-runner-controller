9c27391265493f23290f11e18106ee4a0c373fed
Juho Saarinen <juho.saarinen@gmail.com>
2025-05-17 13:50:11 +0300

    build: move from jest to vitest
    moved test directory to root to prevent automatic transpile by JSII

---
+++ tests/index.test.ts
@@ -0,0 +1,53 @@
+import { Testing } from "cdktf";
+import "cdktf/lib/testing/adapters/jest";
+import { Azure } from "../src/lib/azure";
+import { Gcp } from "../src/lib/gcp";
+import { Aws } from "../src/lib/aws";
+import { describe, expect, it } from "vitest";
+
+
+// To learn more about testing see cdk.tf/testing
+describe("Should synthesize snapshot for construct", () => {
+  describe('without container support on', () => {
+    it("Azure", () => {
+      expect(
+        Testing.synthScope((scope) => {
+          new Azure(scope, "azure");
+        })
+      ).toMatchSnapshot();
+    });
+
+    it("Aws", () => {
+      expect(
+        Testing.synthScope((scope) => {
+          new Aws(scope, "aws", {
+            clusterName: 'test-cluster',
+            containerSupport: false
+          });
+        })
+      ).toMatchSnapshot();
+    });
+
+    it("Google", () => {
+      expect(
+        Testing.synthScope((scope) => {
+          new Gcp(scope, "google");
+        })
+      ).toMatchSnapshot();
+    });
+  });
+
+  describe('with container support on', () => {
+
+    it("Aws", () => {
+      expect(
+        Testing.synthScope((scope) => {
+          new Aws(scope, "aws", {
+            clusterName: 'test-cluster',
+            containerSupport: true
+          });
+        })
+      ).toMatchSnapshot();
+    });
+  })
+});
