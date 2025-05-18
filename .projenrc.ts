ed7e73b8a9e9c8520d32a772429dc23eb4923da7
Juho Saarinen <juho.saarinen@gmail.com>
2025-05-18 17:05:07 +0300

    feat: move to Biome

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,62 +1,72 @@
-import { Vitest } from "@nikovirtala/projen-vitest";
-import { GithubCredentials } from "projen/lib/github";
-import { NodePackageManager } from "projen/lib/javascript";
+import { Vitest } from '@nikovirtala/projen-vitest'
+import { Biome } from 'projen-biome'
 import {
   HybridModule,
   publishToRegistries,
-} from "projen-cdktf-hybrid-construct";
-
+} from 'projen-cdktf-hybrid-construct'
+import { GithubCredentials } from 'projen/lib/github'
+import { NodePackageManager } from 'projen/lib/javascript'
 const project = new HybridModule({
-  author: "Hi-Fi",
-  authorAddress: "juho.saarinen@gmail.com",
-  defaultReleaseBranch: "main",
+  author: 'Hi-Fi',
+  authorAddress: 'juho.saarinen@gmail.com',
+  defaultReleaseBranch: 'main',
   packageManager: NodePackageManager.NPM,
   devDeps: [
-    "projen-cdktf-hybrid-construct",
-    "@aws-sdk/client-ecs",
-    "@nikovirtala/projen-vitest",
+    'projen-cdktf-hybrid-construct',
+    '@aws-sdk/client-ecs',
+    '@nikovirtala/projen-vitest',
+    'projen-biome',
   ],
-  license: "MIT",
-  jsiiVersion: "~5.8.0",
-  constructVersion: "10.4.2",
-  name: "terraform-cdk-serverless-github-actions-runner-controller",
+  license: 'MIT',
+  jsiiVersion: '~5.8.0',
+  constructVersion: '10.4.2',
+  name: 'terraform-cdk-serverless-github-actions-runner-controller',
   projenrcTs: true,
   projenCredentials: GithubCredentials.fromPersonalAccessToken({
-    secret: "GITHUB_TOKEN",
+    secret: 'GITHUB_TOKEN',
   }),
   depsUpgradeOptions: {
     workflow: false,
   },
   renovatebot: true,
   repositoryUrl:
-    "https://github.com/Hi-Fi/terraform-cdk-serverless-github-actions-runner-controller.git",
+    'https://github.com/Hi-Fi/terraform-cdk-serverless-github-actions-runner-controller.git',
   peerDeps: [
-    "@cdktf/provider-azurerm",
-    "@cdktf/provider-random",
-    "@cdktf/provider-aws",
-    "@cdktf/provider-google",
-    "@cdktf/provider-null",
+    '@cdktf/provider-azurerm',
+    '@cdktf/provider-random',
+    '@cdktf/provider-aws',
+    '@cdktf/provider-google',
+    '@cdktf/provider-null',
   ],
   ...publishToRegistries({
-    name: "serverless-github-actions-runner-controller",
-    namespace: "hi-fi",
-    registries: ["npm"],
+    name: 'serverless-github-actions-runner-controller',
+    namespace: 'hi-fi',
+    registries: ['npm'],
   }),
   // use Vitest instead
   jest: false,
+  // use Biome instead
+  eslint: false,
+  prettier: false,
+  testdir: 'tests',
   // cdktfVersion: "0.20.0",        /* Minimum target version of this library. */
   // constructExamples: undefined,  /* If set a construct examples folder will be created. */
   // deps: [],                      /* Runtime dependencies of this module. */
   // description: undefined,        /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,        /* The "name" in package.json. */
   // terraformExamples: undefined,  /* If set a terraform examples folder will be created. */
-});
+})
 
 project.github?.actions.set(
-  "hashicorp/setup-terraform",
-  "hashicorp/setup-terraform@v3",
-);
+  'hashicorp/setup-terraform',
+  'hashicorp/setup-terraform@v3',
+)
 
-new Vitest(project);
+new Vitest(project)
+new Biome(project, {
+  formatter: true,
+  linter: true,
+  organizeImports: true,
+})
 
-project.synth();
+project.synth()
--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,38 +1,40 @@
-import { TerraformVariable } from "cdktf";
-import { Construct } from "constructs";
+import { TerraformVariable } from 'cdktf'
+import { Construct } from 'constructs'
 
 export interface Variables {
-    pat: TerraformVariable;
-    githubConfigUrl: TerraformVariable;
-    autoscalerImage: TerraformVariable;
+  pat: TerraformVariable
+  githubConfigUrl: TerraformVariable
+  autoscalerImage: TerraformVariable
 }
 
 export function commonVariables(scope: Construct): Variables {
-    const pat = new TerraformVariable(scope, 'personal_access_token', {
-        description: 'Github PAT with Actions:Read and Admin:Read+Write scopes',
-        nullable: false,
-        sensitive: true
-    })
+  const pat = new TerraformVariable(scope, 'personal_access_token', {
+    description: 'Github PAT with Actions:Read and Admin:Read+Write scopes',
+    nullable: false,
+    sensitive: true,
+  })
 
-    pat.overrideLogicalId('personal_access_token');
+  pat.overrideLogicalId('personal_access_token')
 
-    const githubConfigUrl = new TerraformVariable(scope, 'github_config_url', {
-        description: 'Github URL where runners should register to. Format https://<GitHub host>/<your_enterprise/org/repo>',
-        nullable: false,
-    })
+  const githubConfigUrl = new TerraformVariable(scope, 'github_config_url', {
+    description:
+      'Github URL where runners should register to. Format https://<GitHub host>/<your_enterprise/org/repo>',
+    nullable: false,
+  })
 
-    githubConfigUrl.overrideLogicalId('github_config_url');
+  githubConfigUrl.overrideLogicalId('github_config_url')
 
-    const autoscalerImage = new TerraformVariable(scope, 'autoscaler_image', {
-        description: 'Autoscaler image triggering runners',
-        default: 'ghcr.io/hi-fi/gha-runners-on-managed-env:ebe559da0d51e04eff903f0a04de09da93f4614f'
-    })
+  const autoscalerImage = new TerraformVariable(scope, 'autoscaler_image', {
+    description: 'Autoscaler image triggering runners',
+    default:
+      'ghcr.io/hi-fi/gha-runners-on-managed-env:ebe559da0d51e04eff903f0a04de09da93f4614f',
+  })
 
-    autoscalerImage.overrideLogicalId('autoscaler_image');
+  autoscalerImage.overrideLogicalId('autoscaler_image')
 
-    return {
-        pat,
-        githubConfigUrl,
-        autoscalerImage
-    }
+  return {
+    pat,
+    githubConfigUrl,
+    autoscalerImage,
+  }
 }
